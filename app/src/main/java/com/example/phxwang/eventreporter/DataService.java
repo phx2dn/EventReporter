b9395e09e51d64b1ab4cf60fd9da9f9aaea08de4
PHX WANG <phx.dn.wang@gmail.com>
2017-09-17 00:39:02 -0500

    Test commit

---
+++ app/src/main/java/com/example/phxwang/eventreporter/DataService.java
@@ -0,0 +1,25 @@
+package com.example.phxwang.eventreporter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by PHX WANG on 6/20/2017.
+ */
+
+public class DataService {
+    /**
+     * Fake all the event data for now. We will refine this and connect
+     * to our backend later.
+     */
+    public static List<Event> getEventData() {
+        List<Event> eventData = new ArrayList<Event>();
+        for (int i = 0; i < 10; ++i) {
+            eventData.add(
+                    new Event("Event", "1184 W valley Blvd, CA 90101",
+                            "This is a huge event"));
+        }
+        return eventData;
+    }
+
+}
